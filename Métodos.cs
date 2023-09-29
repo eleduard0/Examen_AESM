a021a560c2f17d1dce68ac61487c1a373a28598e
eleduard0 <arturo.13040570@gmail.com>
2023-09-29 11:05:12 -0700

    Agregar archivos de proyecto.

---
+++ Métodos.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen_AESM
+{
+    public class Metodos
+    {
+        public void IntervaloDeNumeros(int inicio , int final)
+        {
+            if (inicio < final)
+            {
+                for (int i = inicio; i <= final; i++)
+                {
+                    Console.WriteLine($"La secuencia de números es {i}");
+
+                }
+            }
+
+            else if (inicio > final)
+            {
+                for ( int i = inicio; i >= final; i--)
+                {
+                    Console.WriteLine($"La secuencia de números es {i}");
+                }
+
+            }        
+
+           
+            else Console.WriteLine("Los números son iguales");
+        }
+
+        public void TablaDeMultiplicar(int num1 , int num2 )
+        {
+            if (num2 <= 10)
+            {
+                Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+                TablaDeMultiplicar(num1, num2 + 1);
+
+            }
+
+        }
+
+        public string CURPFecha(string curp)
+        {
+            int longitud = curp.Length;
+            int numeros = longitud / 2;
+
+            return curp.Substring(numeros-5,6);
+
+        }
+        public string CURPSexo(string curp)
+        {
+            int longitud = curp.Length;
+            int numeros = longitud / 2;
+
+            return curp.Substring(numeros +1,1);
+        }
+
+     
+
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Examen_AESM;
+using System.Globalization;
+
+Console.WriteLine("Ejercicio 1:");
+Metodos metodo1 = new Metodos();
+Console.WriteLine("Ingrese 2 numeros");
+int inicio = Convert.ToInt32(Console.ReadLine());
+int fin = Convert.ToInt32(Console.ReadLine());
+
+metodo1.IntervaloDeNumeros(inicio, fin);
+
+Console.WriteLine("\nEjercicio 2: ");
+Console.WriteLine("Ingresa tu curp");
+string curp = Console.ReadLine();
+
+string data = metodo1.CURPFecha(curp).Insert(4, "/").Insert(2, "/");
+
+
+
+if (curp.Length == 18)
+{
+    DateOnly fecha = DateOnly.ParseExact(data, "yy/MM/dd", CultureInfo.InvariantCulture);
+    if (metodo1.CURPSexo(curp) == "M")
+    {
+        Console.WriteLine($"Usted es de sexo femenino y su fecha de nacimiento es: {fecha}");
+    }
+    else Console.WriteLine($"Usted es de sexo masculino y su fecha de nacimiento es: {fecha}");
+
+}
+else Console.WriteLine("La curp debe ser de 18 caracteres");
+
+Console.WriteLine("\nEjercicio 3: ");
+Console.WriteLine("Ingresa que tabla quieres saber: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+
+metodo1.TablaDeMultiplicar(num1, 0);
+
+
