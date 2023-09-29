0eae691922a2a1b1cf31276d90a640a92454ace4
eleduard0 <arturo.13040570@gmail.com>
2023-09-29 11:43:49 -0700

    Examen

--- Métodos.cs
+++ Métodos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -43,21 +44,37 @@ namespace Examen_AESM
 
         }
 
-        public string CURPFecha(string curp)
+        public void CURP(string curp)
         {
-            int longitud = curp.Length;
-            int numeros = longitud / 2;
+            if(curp.Length == 18)
+            {
+                int longitud = curp.Length;
+                int numeros = longitud / 2;
 
-            return curp.Substring(numeros-5,6);
+                string fn = curp.Substring(numeros-5,6);
+                string sexo = curp.Substring(numeros + 1, 1);
+
+                string data = fn.Insert(4, "/").Insert(2, "/");
+                DateOnly fecha = DateOnly.ParseExact(data, "yy/MM/dd", CultureInfo.InvariantCulture);
+                if (sexo == "M")
+                {
+                    Console.WriteLine($"Usted es de sexo femenino y su fecha de nacimiento es: {fecha}");
+                }
+                else Console.WriteLine($"Usted es de sexo masculino y su fecha de nacimiento es: {fecha}");
+
+            }
+            else Console.WriteLine("La curp debe ser de 18 caracteres");
+
+
 
         }
-        public string CURPSexo(string curp)
+        /*public string CURPSexo(string curp)
         {
             int longitud = curp.Length;
             int numeros = longitud / 2;
 
             return curp.Substring(numeros +1,1);
-        }
+        }*/
 
      
 
--- Program.cs
+++ Program.cs
@@ -12,22 +12,7 @@ metodo1.IntervaloDeNumeros(inicio, fin);
 Console.WriteLine("\nEjercicio 2: ");
 Console.WriteLine("Ingresa tu curp");
 string curp = Console.ReadLine();
-
-string data = metodo1.CURPFecha(curp).Insert(4, "/").Insert(2, "/");
-
-
-
-if (curp.Length == 18)
-{
-    DateOnly fecha = DateOnly.ParseExact(data, "yy/MM/dd", CultureInfo.InvariantCulture);
-    if (metodo1.CURPSexo(curp) == "M")
-    {
-        Console.WriteLine($"Usted es de sexo femenino y su fecha de nacimiento es: {fecha}");
-    }
-    else Console.WriteLine($"Usted es de sexo masculino y su fecha de nacimiento es: {fecha}");
-
-}
-else Console.WriteLine("La curp debe ser de 18 caracteres");
+metodo1.CURP(curp);
 
 Console.WriteLine("\nEjercicio 3: ");
 Console.WriteLine("Ingresa que tabla quieres saber: ");
